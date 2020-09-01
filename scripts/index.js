666a6aba6d6890908458d3fa03ea72ef6809f432
Chakrygin Maxim <m.chakrygin@gmail.com>
2020-09-01 10:57:43 +0300

    fix: revue result

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,43 @@
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__button-close');
 const formElement = popup.querySelector('.popup__container');
-const nameInput = popup.querySelector('.popup__name');
-const jobInput = popup.querySelector('.popup__job');
+const nameInput = popup.querySelector('.popup__input_field_name');
+const jobInput = popup.querySelector('.popup__input_field_job');
 const editButton = document.querySelector('.profile__button-edit');
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
 
-const renderEditProfile = () => {
+const renderEditProfile = function () {
     popup.classList.toggle('popup_opened');
-}
+};
 
-const popupCloseByClickOnOverlay = (e)=>{
-    
-    if (e.target == e.currentTarget){
+const popupCloseByClickOnOverlay = function (e) {
+    if (e.target == e.currentTarget) {
         renderEditProfile();
     }
 };
 
-editButton.addEventListener('click', function (e) {
+const closeButtonOnClick = function (){
+    renderEditProfile();
+}
+
+const editButtonOnClick = function () {
     nameInput.value = name.innerText;
     jobInput.value = job.innerText;
 
     renderEditProfile();
-});
+};
 
-closeButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    renderEditProfile();
-});
-
-formElement.addEventListener('submit', function (e) {
+const popupSubmit = function (e) {
     e.preventDefault();
 
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
 
     renderEditProfile();
-});
+};
 
 popup.addEventListener('click', popupCloseByClickOnOverlay);
-
+editButton.addEventListener('click', editButtonOnClick);
+closeButton.addEventListener('click', closeButtonOnClick);
+formElement.addEventListener('submit', popupSubmit);
