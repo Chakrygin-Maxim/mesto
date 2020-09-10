cae78fd2d7b8b3e5e7ab3e762e98707ca45a40d8
Chakrygin Maxim <m.chakrygin@gmail.com>
2020-09-10 11:15:51 +0300

    feat: add initial cards

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,34 @@ const jobInput = popup.querySelector('.popup__input_field_job');
 const editButton = document.querySelector('.profile__button-edit');
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
+const cardsElements = document.querySelector('.elements');
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 const renderEditProfile = function () {
     popup.classList.toggle('popup_opened');
@@ -17,7 +45,7 @@ const popupCloseByClickOnOverlay = function (e) {
     }
 };
 
-const closeButtonOnClick = function (){
+const closeButtonOnClick = function () {
     renderEditProfile();
 }
 
@@ -37,7 +65,23 @@ const popupSubmit = function (e) {
     renderEditProfile();
 };
 
+const addCards = function (cardLink, name) {
+
+    const cardTemplate = document.querySelector('#element-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+
+    const cardPhoto = cardElement.querySelector('.element__photo');
+    cardPhoto.src = cardLink;
+    cardPhoto.alt = name;
+
+    cardElement.querySelector('.element__info-name').textContent = name;
+    
+    cardsElements.append(cardElement);
+};
+
 popup.addEventListener('click', popupCloseByClickOnOverlay);
 editButton.addEventListener('click', editButtonOnClick);
 closeButton.addEventListener('click', closeButtonOnClick);
 formElement.addEventListener('submit', popupSubmit);
+
+initialCards.forEach(card => addCards(card.link, card.name));
