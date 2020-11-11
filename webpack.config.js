53d5cb386b40d93803c9d3b4ab0c7131499d8668
Chakrygin Maxim <m.chakrygin@gmail.com>
2020-11-11 13:56:08 +0300

    feat: добавить распределение файлов по папкам при деплое

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,71 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
+  entry: { main: "./src/pages/index.js" },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/, // Загрузчик JS
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // Загрузчик JS
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /.(png|svg|jpe?g|gif)$/, // Загрузчик картинок
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./images/",
             },
-            {
-                test: /.(png|svg|jpe?g|gif)$/, // Загрузчик картинок
-                use: [
-                    {
-                        loader: 'file-loader?name=./images/[name].[ext]'
-                    }
-                ]
+          },
+        ],
+      },
+      {
+        test: /.(eot|ttf|woff|woff2)$/, // Загрузчик шрифтов
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./vendor/",
             },
-            {
-                test: /.(eot|ttf|woff|woff2)$/, // Загрузчик шрифтов
-                use: [
-                    {
-                        loader: 'file-loader?name=./vendor/[name].[ext]',
-                    }
-                ]
+          },
+        ],
+      },
+      {
+        test: /\.html$/i, // Загрузчик для html
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i, // Загрузчик css
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
             },
-            {
-                test: /\.html$/i, // Загрузчик для html
-                loader: 'html-loader',
-            },
-            {
-                test: /\.css$/i, // Загрузчик css
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                        },
-                    },
-                    'postcss-loader',
-                ],
-            },
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html' }),
-        new MiniCssExtractPlugin(),
+          },
+          "postcss-loader",
+        ],
+      },
     ],
-    devtool: 'inline-source-map',
-}; 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: "inline-source-map",
+};
